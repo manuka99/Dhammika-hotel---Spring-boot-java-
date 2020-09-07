59319e2965fefb63d30603b920d4bc64ba0b0fd4
manukayasas94@gmail.com <manukad1>
2020-09-08 01:09:40 +0530

    progressql error fixed

--- src/main/java/com/hotel/management/repository/UserRepository.java
+++ src/main/java/com/hotel/management/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.hotel.management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hotel.management.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
 	User findByEmail(String email);
